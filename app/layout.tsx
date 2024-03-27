b5207897e2e78367a2f5c632f7bc5660fe474819
sohagzayan <gdsohag360@gmail.com>
2024-03-27 11:31:25 +0600

    remove attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
         <Provider session={session}>
           <TooltipProvider>
             <ReduxProvider>
-              <div className=''>
+              <div >
                 {children}
               </div>
             </ReduxProvider>
