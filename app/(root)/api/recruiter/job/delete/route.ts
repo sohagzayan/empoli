6d9d06ee1ac4432bce1cee00853b1bd1e1301031
sohagzayan <gdsohag360@gmail.com>
2024-09-22 02:10:13 +0600

    remoce api

--- app/(root)/api/recruiter/job/delete/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/lib/database";
-import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
-// import { v4 as uuid } from 'uuid'
-
-export async function DELETE(request: Request) {
-  return NextResponse.json({ message: "delete" }, { status: 500 });
-}
