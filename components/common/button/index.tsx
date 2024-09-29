4ed6ab4b2becf5e3cfc244f791ca381a43693534
sohagzayan <gdsohag360@gmail.com>
2024-09-30 00:34:39 +0600

    edit

--- components/common/button/index.tsx
+++ components/common/button/index.tsx
@@ -2,39 +2,42 @@ import clsx from 'clsx';
 import React from 'react';
 
 type ButtonProps = {
-    variant?: 'primary' | 'secondary' | 'danger'; // You can add more variants
-    text: string;
-    onClick?: () => void;
-    className?: string; // Allows for additional custom Tailwind CSS classes
-    type?: 'button' | 'submit' | 'reset';
+  variant?: 'primary' | 'secondary' | 'danger'; // You can add more variants
+  text: string;
+  onClick?: () => void;
+  className?: string; // Allows for additional custom Tailwind CSS classes
+  type?: 'button' | 'submit' | 'reset';
 };
 
 const Button: React.FC<ButtonProps> = ({
-    variant = 'primary',
-    text,
-    onClick,
-    className = '',
-    type = 'button',
+  variant = 'primary',
+  text,
+  onClick,
+  className = '',
+  type = 'button',
 }) => {
-    // Define base and variant-specific styles
-    const baseClasses =
-        'transition-all ease-in-out duration-300 px-6 py-2 font-500 rounded-md';
+  // Define base and variant-specific styles
+  const baseClasses =
+    'transition-all ease-in-out duration-300 px-7 py-2 font-500 rounded-md';
 
-    const variants: Record<string, string> = {
-        primary: 'border-2 border-theme1 bg-transparent hover:bg-theme1 text-theme1 hover:text-white',
-        secondary: 'border-2 border-theme1 bg-theme1 hover:bg-transparent text-white hover:text-theme1',
-        danger: 'border-2 border-red-500 bg-red-500 hover:bg-transparent text-white hover:text-red-500',
-    };
+  const variants: Record<string, string> = {
+    primary:
+      'border-2 border-theme1 bg-transparent hover:bg-theme1 text-theme1 hover:text-white',
+    secondary:
+      'border-2 border-theme1 bg-theme1 hover:bg-transparent text-white hover:text-theme1',
+    danger:
+      'border-2 border-red-500 bg-red-500 hover:bg-transparent text-white hover:text-red-500',
+  };
 
-    return (
-        <button
-            type={type}
-            onClick={onClick}
-            className={clsx(baseClasses, variants[variant], className)}
-        >
-            {text}
-        </button>
-    );
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={clsx(baseClasses, variants[variant], className)}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
